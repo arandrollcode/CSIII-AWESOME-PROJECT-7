5bea0d7845efaf86bac117ad3fae5628f47651d8
nathan <s119117@student.roundrockisd.org>
2024-09-13 13:40:46 -0500

    sdf

--- main.java
+++ main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("sdfsdfsdf");
-=======
         System.out.println("peepee");
->>>>>>> c7bdb3d2c288b8e670bc014ef3c6ca7669eafc7b
     }
 }
 
