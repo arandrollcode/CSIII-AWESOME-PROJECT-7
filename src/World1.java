28e5d9a99cf6e1f4790e4289b61a9387d584e482
Nathan <pianobear2008@gmail.com>
2024-09-19 13:54:11 -0500

    restructure main character tracking

--- src/World1.java
+++ src/World1.java
@@ -4,12 +4,12 @@ public class World1 extends World {
    public World1() {
       Mayflower.showBounds(true);
       setBackground("src/img/BG/BG.png");
-      Cat cat = new Cat();
-      addObject(cat, 150, 350);
-      addObject(new Block(cat), 100, 500);
-      addObject(new Block(cat), 228, 372);
-      addObject(new Block(cat), 400, 250);
-      addObject(new Block(cat), 500, 100);
+      MovableAnimatedActor player = StartScreen.getPlayer();
+      addObject(player, 150, 350);
+      addObject(new Block(), 100, 500);
+      addObject(new Block(), 228, 372);
+      addObject(new Block(), 400, 250);
+      addObject(new Block(), 500, 100);
 
       Mayflower.playMusic("src/sounds/music/music.mp3");
    }
