554c5cef642833582de46802d8fec86b0a2d5f39
Nathan <pianobear2008@gmail.com>
2024-09-23 14:25:21 -0500

    initial commit for other worlds

--- src/MyMayflower.java
+++ src/MyMayflower.java
@@ -2,7 +2,7 @@ import mayflower.*;
 
 public class MyMayflower extends Mayflower {
    public MyMayflower() {
-      super("PROJECT", 800, 600);
+      super("Epic Game", 800, 600);
    }
 
    public void init() {
