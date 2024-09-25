856785dc0c029174e213a9dd7482b27d04821c7b
connor <connorhall365@gmail.com>
2024-09-25 13:32:02 -0500

    Merge branch 'main' into Connor

--- src/Block.java
+++ src/Block.java
@@ -3,7 +3,9 @@ import mayflower.MayflowerImage;
 public class Block extends Platform {
 
    public Block() {
-      setImage("src/img/Tiles/2.png");
+      MayflowerImage tile = new MayflowerImage("src/img/Tiles/2.png");
+      tile.scale(100, 100);
+      setImage(tile);
    }
 
 }
