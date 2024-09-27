b0ab208152e858d9a3295f209ab57231342dc381
david <s125745@student.roundrockisd.org>
2024-09-27 13:40:30 -0500

    block graphics

--- src/Block.java
+++ src/Block.java
@@ -5,7 +5,7 @@ public class Block extends Platform {
 
    public Block() {
       super();
-      MayflowerImage tile = new MayflowerImage("src/img/Tiles/2.png");
+      MayflowerImage tile = new MayflowerImage("src/img/sprite/block.png");
       tile.scale(100, 100);
       setImage(tile);
    }
--- src/StartScreen.java
+++ src/StartScreen.java
@@ -5,14 +5,13 @@ public class StartScreen extends World {
 
     public StartScreen() {
         setBackground("src/img/BG/gamingwall.png");
-        // showText("Welcome to EPIC GAMING GAME", 45, 0, 200, Color.RED);
-        // showText("Press [ENTER] to Start", 45, 0, getHeight() / 2, Color.BLACK);
     }
 
+    // When enter is pressed, go from starting screen to world 1
     public void act() {
         if (Mayflower.isKeyDown(Keyboard.KEY_ENTER)) {
             playerCharacter = new Player();
-            Mayflower.setWorld(new World3());
+            Mayflower.setWorld(new World1());
         }
     }
 
