3d9c3c933d1c37b55c6ee9aec6f61154b8b0b953
david <s125745@student.roundrockisd.org>
2024-10-01 13:56:31 -0500

    bossgraphics

--- src/DeathScreen.java
+++ src/DeathScreen.java
@@ -4,6 +4,7 @@ public class DeathScreen extends World {
     public DeathScreen() {
         setBackground("src/img/BG/deathscreen.png");
         Mayflower.stopMusic("src/sounds/music/music.mp3");
+        Mayflower.stopMusic("src/sounds/music/boss.mp3");
         Mayflower.playMusic("src/sounds/sadtrombone.mp3");
     }
 
